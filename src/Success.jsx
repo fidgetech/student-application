9f4493f9da2d7f9e96ef4cc7dddcf31ad5e47cab
Mike Goren <mike@mortalwombat.net>
2024-10-11 16:30:03 -0700

    scroll to top on success component display

--- src/Success.jsx
+++ src/Success.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import Header from './Header.jsx';
 
 function Success({ heading='Success!', subHeading='Thank you for submitting your application.' }) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    window.parent.postMessage({ height: document.documentElement.scrollHeight }, '*');
+  }, []);
+
   return <Header heading={heading} subHeading={subHeading} />;
 }
 
