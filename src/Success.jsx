09b2cc94abbfde41897b6b46c16d99285bf08e87
Mike Goren <mike@mortalwombat.net>
2024-10-11 16:37:41 -0700

    wait on scroll until dom renders

--- src/Success.jsx
+++ src/Success.jsx
@@ -3,8 +3,10 @@ import Header from './Header.jsx';
 
 function Success({ heading='Success!', subHeading='Thank you for submitting your application.' }) {
   useEffect(() => {
-    window.scrollTo(0, 0);
-    window.parent.postMessage({ height: document.documentElement.scrollHeight }, '*');
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+      window.parent.postMessage({ height: document.documentElement.scrollHeight }, '*');
+    }, 0);
   }, []);
 
   return <Header heading={heading} subHeading={subHeading} />;
