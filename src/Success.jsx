8d03ff9da9e37a147cd3c8caa2fd03abfd25ebca
Mike Goren <mike@mortalwombat.net>
2024-10-11 16:41:24 -0700

    attempt another approach for scroll on success

--- src/Success.jsx
+++ src/Success.jsx
@@ -3,10 +3,10 @@ import Header from './Header.jsx';
 
 function Success({ heading='Success!', subHeading='Thank you for submitting your application.' }) {
   useEffect(() => {
-    setTimeout(() => {
+    requestAnimationFrame(() => {
       window.scrollTo(0, 0);
       window.parent.postMessage({ height: document.documentElement.scrollHeight }, '*');
-    }, 0);
+    });
   }, []);
 
   return <Header heading={heading} subHeading={subHeading} />;
